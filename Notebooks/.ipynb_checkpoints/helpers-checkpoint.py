5ca0d1a39139001616d6abf46e3649edaf1f2c2f
Oceanos1 <evondom@hotmail.com>
2021-06-09 02:40:41 +0200

    big commit including all the supervised iteration work but later, only the last iteration will be kept, and many more files

--- Notebooks/.ipynb_checkpoints/helpers-checkpoint.py
+++ Notebooks/.ipynb_checkpoints/helpers-checkpoint.py
@@ -17,7 +17,7 @@ from flowsom import *
 from sklearn.cluster import AgglomerativeClustering
 import seaborn as sns;
 import matplotlib.pyplot as plt
-
+import collections
 
 from data import *
 
@@ -77,29 +77,26 @@ def create_labeled_dataset(directory,columns,folder_output='/../labeled_dataset/
             #to_csv(df_labeled,dir_path+'/labeled_dataset/'+new_filename)
 
 def preprocess(file,columns):
-    print(file)
+    #print(file)
     df_labeled = pd.read_csv(file)
 
     #creation of X and y
     X,y = split_input_output(df_labeled,target_feature='label')
-    
     X = X[columns]
     
+    #Index of all values strictly positive (because we will apply log() to all our data for standardization)
     na_indexes = (X > 0).all(1)
 
-
     #Standardize our data
-    X = np.log(X)
     X = X[na_indexes]
     y = y[na_indexes]
-    
+    X = np.log(X)
+
     scaler = preprocessing.StandardScaler()
-    X[X.columns] = scaler.fit_transform(X[X.columns])
+    X[columns] = scaler.fit_transform(X[columns])
     
-        
     #Detect and remove outliers
     X,y = remove_outliers_with_y(X,y)
-
     
     #Save file in csv format
     save_to_csv(X,y)
@@ -108,8 +105,8 @@ def preprocess(file,columns):
 def run_eval(X, y_true,y_pred):
     print("Rand Index: %0.3f" % metrics.rand_score(y_true, y_pred))
     print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(y_true, y_pred))
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(y_true, y_pred))
-    print("Completeness: %0.3f" % metrics.completeness_score(y_true, y_pred))
+    #print("Homogeneity: %0.3f" % metrics.homogeneity_score(y_true, y_pred))
+    #print("Completeness: %0.3f" % metrics.completeness_score(y_true, y_pred))
     print("V-measure: %0.3f" % metrics.v_measure_score(y_true, y_pred))
     print("Adjusted Mutual Information: %0.3f" % metrics.adjusted_mutual_info_score(y_true, y_pred))
 #    print("Silhouette Coefficient: %0.3f" % #metrics.silhouette_score(X, y_pred))
@@ -133,7 +130,7 @@ def run_FlowSOM(file):
     fsom.labeling()
     return fsom.df
             
-def run_all(columns,directory='../labeled_dataset/',output_directory='/outputs/',clustering="kmeans"):
+def run_all(columns,directory='../labeled_dataset/',output_directory='/outputs/',clustering="kmeans",verbose=False):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     for entry in os.scandir(dir_path+"/"+directory):
         if not entry.is_file:
@@ -141,45 +138,45 @@ def run_all(columns,directory='../labeled_dataset/',output_directory='/outputs/'
         elif entry.path.endswith(".csv") and entry.is_file():
             filename= entry.name
             X,y = preprocess(directory+filename,columns)
-            print(entry.path)
-            print(X.shape)
-            #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-            X_train = X.copy()
-            X_test = X.copy()
-            y_train = y.copy()
-            y_test = y.copy()
             command = clustering.lower()
             if(command == "flowgrid"):
                 save_to_csv(X,y)
                 run_FlowGrid()
                 y_pred = np.genfromtxt('fc_data_FlowGrid_label.csv', delimiter=',')
-                X_test = X_test.drop([0])
-                y_test = y_test.drop([0])
+                X = X.drop([0])
+                y = y.drop([0])
             elif(command == "flowsom"):
                 save_to_csv(X,y,X_name="flowsom.csv",y_name="flowsom_label.csv")
                 output_df = run_FlowSOM('flowsom.csv')
                 y_pred = output_df['category']
-                X_test = X_test.drop([len(y)-1])
-                y_test = y_test.drop([len(y)-1])
+                X = X.drop([0])
+                y = y.drop([0])
             elif(command == "kmeans"):
-                cluster_model = clusterKMeans(X_train)
-                y_pred = cluster_model.predict(X_test)
+                cluster_model = clusterKMeans(X)
+                y_pred = cluster_model.labels_
             elif(command =="gmm"):
-                cluster_model = clusterGMM(X_train)
-                y_pred = cluster_model.predict(X_test)
+                cluster_model = clusterGMM(X)
+                y_pred = cluster_model.labels_
             elif(command =="optics"):
-                cluster_model = clusterOPTICS(X_test)
+                cluster_model = clusterOPTICS(X)
                 y_pred = cluster_model.labels_
             elif(command =="dbscan"):
-                cluster_model = clusterDBSCAN(X_test)
+                cluster_model = clusterDBSCAN(X)
                 y_pred = cluster_model.labels_
             else:
                 print(f"Unknown clustering algorithm")
                 break
-            run_eval(X_test,y_test,y_pred)
+            print(entry.path)
+            run_eval(X,y,y_pred)
             plt.figure()
             plt.title(entry.name + " - " + command)
-            pl = sns.scatterplot(data=X_test, x="B530-H", y="B572-H", hue=y_pred)
+            pl = sns.scatterplot(data=X, x="B530-H", y="B572-H", hue=y_pred)
+            if verbose:
+                print(X.shape)
+                print("real clusters")
+                print(collections.Counter(y).most_common)
+                print("prediction clusters")
+                print(collections.Counter(y_pred).most_common)
             
             new_filename = filename.split(".")[0]
             pl.figure.savefig(dir_path+output_directory+new_filename+"_"+command+".png")
@@ -264,7 +261,7 @@ def get_centroids(data,assignment):
   Returns :
     An array of assignment for each sample to a cluster
 """
-def clusterKMeans(data,n_clusters=2, random_state=0):
+def clusterKMeans(data,n_clusters=3, random_state=0):
     return KMeans(n_clusters=n_clusters, random_state=random_state).fit(data)
 
 
@@ -276,7 +273,7 @@ def clusterKMeans(data,n_clusters=2, random_state=0):
   Returns :
     An array of assignment for each sample to a cluster
 """
-def clusterDBSCAN(data,eps=0.7, min_samples=188,metric='euclidean', algorithm='auto'):
+def clusterDBSCAN(data,eps=0.38, min_samples=170,metric='l2', algorithm='auto'):
     return DBSCAN(eps=eps, min_samples=min_samples, metric=metric, algorithm=algorithm).fit(data)
 
 """
@@ -284,8 +281,8 @@ def clusterDBSCAN(data,eps=0.7, min_samples=188,metric='euclidean', algorithm='a
   Returns :
     An array of assignment for each sample to a cluster
 """
-def clusterOPTICS(data,metric="euclidean"):
-    return OPTICS(metric=metric).fit(data)
+def clusterOPTICS(data,metric="euclidean",min_samples=22,eps=2.0,cluster_method='xi'):
+    return OPTICS(metric=metric,max_eps=eps, min_samples=min_samples,cluster_method=cluster_method).fit(data)
 
 """
   Run the Gaussian Mixture algorithm on the dataset
@@ -318,46 +315,3 @@ def data_filter(filter, data, assignments):
 
     return (np.array(new_data), np.array(new_assignments))
 
-
-def plot_results(data, channels, assignments):
-    for i in range(len(channels)):
-        for j in range(i+1, len(channels)):
-            plt.figure(figsize=(15,7.5))
-
-            plt1 = plt.subplot(1,2,1)
-            dot_plot_personnalized(data, i, j, plot=plt1)
-            plt.gca().set_xlabel(channels[i])
-            plt.gca().set_ylabel(channels[j])
-            plt.gca().set_xlim(-3,3)
-            plt.gca().set_ylim(-3,3)
-
-            plt2 = plt.subplot(1,2,2)
-            dot_plot_personnalized(data, i ,j, colors=assignments)
-            plt.gca().set_xlabel(channels[i])
-            plt.gca().set_ylabel(channels[j])
-            plt.gca().set_xlim(-3,3)
-            plt.gca().set_ylim(-3,3)
-
-            plt.show()
-
-
-def plot_results_b530_b572(data, channels, assignments):
-    plt.figure(figsize=(15,7.5))
-    plt1 = plt.subplot(1,2,1)
-    dot_plot_personnalized(data, 0, 3, plot=plt1)
-    plt.gca().set_xlabel('B530-H')
-    plt.gca().set_ylabel('B572-H')
-#    plt.gca().set_xlim(-3,3)
-#    plt.gca().set_ylim(-3,3)
-
-    plt2 = plt.subplot(1,2,2)
-    dot_plot_personnalized(data, 0 ,3, colors=assignments)
-    plt.gca().set_xlabel('B530-H')
-    plt.gca().set_ylabel('B572-H')
-#    plt.gca().set_xlim(-3,3)
-#    plt.gca().set_ylim(-3,3)
-    
-    plt.show()
-
-
-        
